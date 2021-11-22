f1fe98331ce242008f8bd8d0508dac2c21b7ee87
daniel-kramt <47295556+daniel-kramt@users.noreply.github.com>
2021-11-22 12:54:58 +0100

    Create corrected_api_call.R
    initial commit

---
+++ corrected_api_call.R
@@ -0,0 +1,17 @@
+library(httr)
+
+url <- "https://rewriter-paraphraser-text-changer-multi-language.p.rapidapi.com/rewrite"
+
+payload <- "{\r
+    \"language\": \"en\",\r
+    \"strength\": 3,\r
+    \"text\": \"Rewriting text is fun!\"\r
+}"
+
+encode <- "json"
+
+# Replace ###### with your API key
+response <- VERB("POST", url, body = text_r(), add_headers(`x-rapidapi-host` = 'rewriter-paraphraser-text-changer-multi-language.p.rapidapi.com', `x-rapidapi-key` = '######'), content_type("application/json"), encode = encode)
+
+tmp <- content(response, "parsed")
+tmp$rewrite
